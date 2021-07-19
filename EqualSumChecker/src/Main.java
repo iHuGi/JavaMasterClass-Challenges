a5e086ccaba3681f712af6f92e6791ce4decbb27
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-07-19 16:11:52 +0100

    EqualSumChecker-Challenge6
    Full EqualSumChecker Challenge Complete.

---
+++ EqualSumChecker/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean equalSum = EqualSumChecker.hasEqualSum(100, 100, 200);
+        System.out.println(equalSum);
+    }
+}
