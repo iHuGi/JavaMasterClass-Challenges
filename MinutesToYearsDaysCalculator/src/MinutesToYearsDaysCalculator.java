1f1815d3a39fb0dc032679465fbe7d6543eaa43e
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-08-11 22:04:02 +0100

    MinusToYearsDaysCalculator - Challenge9
    Full MinusToYearsDaysCalculator Challenge Complete

---
+++ MinutesToYearsDaysCalculator/src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,15 @@
+// Code Test for Challenge
+
+public class MinutesToYearsDaysCalculator {
+
+        public static void printYearsAndDays (long minutes) {
+            if (minutes < 0) {
+                System.out.println("Invalid Value");
+            } else {
+                long days = ((int) minutes / 60) / 24 ;
+                long years = (int) days / 365;
+                long remainingDays = days % 365;
+                System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+            }
+        }
+}
