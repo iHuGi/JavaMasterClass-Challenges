b30bae12ace3375e7613ec44a6296ab147d9921a
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-09-26 07:25:10 +0100

    DiagonalStar-Challenge26
    Full DiagonalStar Challenge Complete.

---
+++ DiagonalStar/src/academy/learnprogramming/Main.java
@@ -0,0 +1,29 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSquareStar(5);
+        System.out.println("SPACE");
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar(int number) {
+        if(number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for(int row = 0; row < number; row++) {
+            for(int col = 0; col < number; col++) {
+                if(row == 0 || row == number - 1 || col == 0 || col == number - 1
+                        || row == col || row + col == number - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
