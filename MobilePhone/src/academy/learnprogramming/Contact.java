fe61e52488ec029eca40a2bd8c81b36d8e602249
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-09-28 08:55:43 +0100

    MobilePhone-Challenge44
    Full MobilePhone Challenge Complete.

---
+++ MobilePhone/src/academy/learnprogramming/Contact.java
@@ -0,0 +1,48 @@
+package academy.learnprogramming;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact (String name, String phoneNumber) {
+        return new Contact (name, phoneNumber);
+    }
+}
+
+///// On Udemy there might be some errors with this code, copy the following that i made for there:
+
+/*public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+}*/
+
