b06b93ab079111b3c3d615142e319f290a646017
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-09-28 08:38:03 +0100

    PoolArea-Challenge36
    Full PoolArea Challenge Complete.

---
+++ PoolArea/src/academy/learnprogramming/Rectangle.java
@@ -0,0 +1,45 @@
+package academy.learnprogramming;
+
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if (width < 0) {
+            this.width = 0;
+            if (length < 0) {
+                this.length = 0;
+            }
+        } else {
+            this.width = width;
+            this.length = length;
+        }
+    }
+
+    // OR (FOR UDEMY)
+
+//     if(width < 0) {
+//        this.width = 0;
+//    } else {
+//        this.width = width;
+//    }
+//
+//        if(length < 0) {
+//        this.length = 0;
+//    } else {
+//        this.length = length;
+//    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea () {
+        return width * length;
+    }
+
+}
