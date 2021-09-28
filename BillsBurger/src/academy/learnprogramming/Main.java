914b4ec8c456e3209404f8975345e91361c9c1e3
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-09-28 08:51:25 +0100

    BillsBurger-Challenge40
    Full BillsBurger Challenge Complete.

---
+++ BillsBurger/src/academy/learnprogramming/Main.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", 3.56, "Sausage", "White");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+        System.out.println("************************************");
+
+        HealthyBurger healthyBurger = new HealthyBurger(5.67, "Bacon");
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthyAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is  " + healthyBurger.itemizeHamburger());
+
+        System.out.println("************************************");
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        System.out.println("Total Deluxe Burger price is " + db.itemizeHamburger());
+    }
+}
