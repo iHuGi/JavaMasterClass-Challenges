f4e59e931f8ef2c45715e13e27c3af0251c568f9
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-09-28 08:35:06 +0100

    CarpetCostCalculator-Challenge33
    Full CarpetCostCalculator Challenge Complete.

---
+++ CarpetCostCalculator/src/academy/learnprogramming/Carpet.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class Carpet {
+
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost;
+        if (this.cost < 0) {
+            this.cost = 0;
+        }
+    }
+
+    public double getCost () {
+        return cost;
+    }
+}
